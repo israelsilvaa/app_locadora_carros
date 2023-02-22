13c025b6a9c4fa84c0b7a199f2fd22a3093cfd8c
israel silva <Israel524.is@gmail.com>
2023-02-22 17:23:01 -0300

    aula 321
    Repository Design Pattern - Implementando um Repository para Modelo

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use App\Models\Modelo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use App\Repositories\ModeloRepository;
 
 class ModeloController extends Controller
 {
     protected $modelo;
-    public function __construct(Modelo $modelo){
+    public function __construct(Modelo $modelo)
+    {
         $this->modelo = $modelo;
     }
     /**
@@ -19,32 +21,23 @@ class ModeloController extends Controller
      */
     public function index(Request $request)
     {
-        $modelos = array();
-        if($request->has('atributos_marca')){
-            $atributos_marca = $request->atributos_marca;
-            $modelos = $this->modelo->with('marca:id,'.$atributos_marca);
-        }else{
-            $modelos = $this->modelo->with('marca');
-        }
-        
-        if($request->has('filtro')){
-            $filtros = explode(';', $request->filtro);
-            
-            foreach($filtros as $key => $condicao){
-                $c = explode(':', $condicao);
-                $modelos = $modelos->where($c[0],$c[1],$c[2]);
-            }
+        $modeloRepository = new ModeloRepository($this->modelo);
+
+        if ($request->has('atributos_marca')) {
+            $atributos_marca = 'marca:id,' . $request->atributos_marca;
+            $modeloRepository->selectAtributosRegistrosRelacionados($atributos_marca);
+        } else {
+            $modeloRepository->selectAtributosRegistrosRelacionados('marca');
         }
 
-        if($request->has('atributos')){
-            $atributos = $request->atributos;    
-            $modelos = $modelos->selectRaw($atributos)->get();
-        }else{
-            $modelos = $modelos->get();
+        if ($request->has('filtro')) {
+            $modeloRepository->filtro($request->filtro);
         }
-        return response()->json($modelos, 200);
-        // ALL() -> PBJ DE CONSULTA + GET() == COLLECTION
-        // get() -> modificar a consulta -> collection 
+        if ($request->has('atributos')) {
+            $modeloRepository->selectAtributos($request->atributos);
+        }
+
+        return response()->json($modeloRepository->getResultado(), 200);
     }
 
     /**
@@ -68,10 +61,10 @@ class ModeloController extends Controller
         // $modelo = Modelo::create($request->all());
         $request->validate($this->modelo->rules());
         // stateless
-        
+
         $imagem = $request->file('imagem');
         $imagem_urn = $imagem->store('imagens/modelos', 'public');
-        
+
         $modelo = $this->modelo->create([
             'marca_id' => $request->marca_id,
             'nome' => $request->nome,
@@ -95,12 +88,12 @@ class ModeloController extends Controller
      */
     public function show($id)
     {
-         // php artisan storage:link
-         $modelo = $this->modelo->with('marca')->find($id);
-         if($modelo === null){
-             return response()->json(['erro'=>'nada encontrado no banco de dados'], 404);
-         }
-         return response()->json($modelo, 200);
+        // php artisan storage:link
+        $modelo = $this->modelo->with('marca')->find($id);
+        if ($modelo === null) {
+            return response()->json(['erro' => 'nada encontrado no banco de dados'], 404);
+        }
+        return response()->json($modelo, 200);
     }
 
     /**
@@ -125,34 +118,33 @@ class ModeloController extends Controller
     {
         $modelo = $this->modelo->find($id);
 
-        if($modelo === null) {
+        if ($modelo === null) {
             return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe'], 404);
         }
 
-        if($request->method() === 'PATCH') {
+        if ($request->method() === 'PATCH') {
 
             $regrasDinamicas = array();
 
             //percorrendo todas as regras definidas no Model
-            foreach($modelo->rules() as $input => $regra) {
-                
+            foreach ($modelo->rules() as $input => $regra) {
+
                 //coletar apenas as regras aplicáveis aos parâmetros parciais da requisição PATCH
-                if(array_key_exists($input, $request->all())) {
+                if (array_key_exists($input, $request->all())) {
                     $regrasDinamicas[$input] = $regra;
                 }
             }
-            
-            $request->validate($regrasDinamicas);
 
+            $request->validate($regrasDinamicas);
         } else {
             $request->validate($modelo->rules());
         }
-        
+
         //remove o arquivo antigo caso um novo arquivo tenha sido enviado no request
-        if($request->file('imagem')) {
+        if ($request->file('imagem')) {
             Storage::disk('public')->delete($modelo->imagem);
         }
-        
+
         $imagem = $request->file('imagem');
         $imagem_urn = $imagem->store('imagens/modelos', 'public');
 
@@ -182,17 +174,17 @@ class ModeloController extends Controller
     public function destroy($id)
     {
         $modelo = $this->modelo->find($id);
-        if($modelo === null){
+        if ($modelo === null) {
             return response()->json([
-                'erro'=> 
-                'a exclusão não pode ser feita pois o recurso não existe'], 404);
+                'erro' =>
+                'a exclusão não pode ser feita pois o recurso não existe'
+            ], 404);
         }
 
         // remove o arquivo antigo
         Storage::disk('public')->delete($modelo->imagem);
-        
+
         $modelo->delete();
-        return response()->json(['msg'=>'O modelo foi deletado com sucesso!'], 200);
-    
+        return response()->json(['msg' => 'O modelo foi deletado com sucesso!'], 200);
     }
 }
