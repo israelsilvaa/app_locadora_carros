5f1d410cc55f7290a5fa5952bdc0300082207fea
israel silva <Israel524.is@gmail.com>
2023-02-26 11:56:15 -0300

    aula 364
    Extra - Evitando dependências nos componentes Vue

--- public/js/app.js
+++ public/js/app.js
@@ -5782,7 +5782,7 @@ __webpack_require__.r(__webpack_exports__);
       nomeMarca: '',
       arquivoImagem: [],
       transacaoStatus: '',
-      transacaoDetalhes: []
+      transacaoDetalhes: {}
     };
   },
   methods: {
@@ -5804,12 +5804,16 @@ __webpack_require__.r(__webpack_exports__);
       };
       axios.post(this.urlBase, formData, config).then(function (response) {
         _this.transacaoStatus = 'adicionado';
-        _this.transacaoDetalhes = response;
-        console.log('RESPONSEEEEE');
+        _this.transacaoDetalhes = {
+          mensagem: 'ID do registro: ' + response.data.id
+        };
         console.log(response);
       })["catch"](function (errors) {
         _this.transacaoStatus = 'erro';
-        _this.transacaoDetalhes = errors.response;
+        _this.transacaoDetalhes = {
+          mensagem: errors.response.data.message,
+          dados: errors.response.data.errors
+        };
         // errors.response.data.message
       });
     }
@@ -29229,20 +29233,14 @@ var render = function () {
     _vm._v("\n    " + _vm._s(_vm.titulo) + "\n    "),
     _c("hr"),
     _vm._v(" "),
-    _vm.detalhes.data.message
-      ? _c("span", [_vm._v(_vm._s(_vm.detalhes.data.message))])
-      : _vm._e(),
-    _vm._v(" "),
-    _vm.detalhes.data.id
-      ? _c("span", [_vm._v(_vm._s("ID do registro: " + _vm.detalhes.data.id))])
-      : _vm._e(),
+    _c("p", [_vm._v(_vm._s(_vm.detalhes.mensagem))]),
     _vm._v(" "),
     _c("br"),
     _vm._v(" "),
-    _vm.detalhes.data.errors
+    _vm.detalhes.dados
       ? _c(
           "ul",
-          _vm._l(_vm.detalhes.data.errors, function (e, key) {
+          _vm._l(_vm.detalhes.dados, function (e, key) {
             return _c("li", { key: key }, [_vm._v(_vm._s(e[0]))])
           }),
           0
--- resources/js/components/marcas.vue
+++ resources/js/components/marcas.vue
@@ -100,7 +100,9 @@ export default {
             nomeMarca: '',
             arquivoImagem: [],
             transacaoStatus: '',
-            transacaoDetalhes: []
+            transacaoDetalhes: {
+
+            }
         }
     },
     methods: {
@@ -125,13 +127,17 @@ export default {
             axios.post(this.urlBase, formData, config)
                 .then(response => {
                     this.transacaoStatus = 'adicionado'
-                    this.transacaoDetalhes = response 
-                    console.log('RESPONSEEEEE')
+                    this.transacaoDetalhes = {
+                        mensagem:'ID do registro: '+ response.data.id
+                    } 
                     console.log(response)
                 })
                 .catch(errors => {
                     this.transacaoStatus = 'erro'
-                    this.transacaoDetalhes= errors.response 
+                    this.transacaoDetalhes= {
+                        mensagem: errors.response.data.message ,
+                        dados: errors.response.data.errors
+                    } 
                     // errors.response.data.message
                 })
         }
